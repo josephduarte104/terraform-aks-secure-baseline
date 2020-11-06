387971dcb2cc73642ad2cd563071e0fb6f4cc3f6
Ubuntu <azureuser@jumpboxvm.2mwjeirxjqqelnlc4jj2323tsg.bx.internal.cloudapp.net>
2020-11-06 21:42:38 +0000

    added test script

---
+++ k8s/test-internal-lb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create a namespace for your ingress resources
+kubectl create namespace ingress-basic
+
+# Add the ingress-nginx repository
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+# Use Helm to deploy an NGINX ingress controller
+helm install nginx-ingress ingress-nginx/ingress-nginx \
+    --namespace ingress-basic \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set controller.service.annotations."beta\.kubernetes\.io/azure-load-balancer-internal"=true \
+    --set controller.service.loadBalancerIP=192.168.4.4
