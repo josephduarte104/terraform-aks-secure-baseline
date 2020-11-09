f6d7fc510beb70566117af08f0ff60f75e03277d
Ubuntu <azureuser@jumpboxvm.bkxesysisqeefmunarg5gjgghe.bx.internal.cloudapp.net>
2020-11-09 21:37:11 +0000

    added test script and removed several unused files

--- k8s/test-internal-lb.sh
+++ k8s/test-internal-lb.sh
@@ -28,6 +28,7 @@ if [ $BLUE = "true" ]; then
 	# Use Helm to deploy an NGINX ingress controller
 	helm install ingress-blue ingress-nginx/ingress-nginx -f - \
 	    --namespace nginx \
+            --set controller.ingressClass=blue \
 	    --set controller.replicaCount=2 \
 	    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
 	    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
@@ -35,9 +36,79 @@ if [ $BLUE = "true" ]; then
             --set controller.nodeSelector.nodepoolcolor="blue" << EOF
 controller:
   service:
-    annotations:
+    annotations
       service.beta.kubernetes.io/azure-load-balancer-internal: "true"
       service.beta.kubernetes.io/azure-load-balancer-internal-subnet: "clusteringressservices"
+EOF
+
+	cat <<EOF | kubectl apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-blue-dep
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: nginx-blue
+  template:
+    metadata:
+      labels:
+        app: nginx-blue
+    spec:
+      nodeSelector:
+        nodepoolcolor: blue
+        nodepoolmode: user
+      containers:
+      - image: nginxdemos/hello
+        name: nginx-blue
+        ports:
+        - containerPort: 80
+        resources:
+          requests:
+            memory: "64Mi"
+            cpu: "250m"
+          limits:
+            memory: "128Mi"
+            cpu: "350m"
+---
+
+apiVersion: v1
+kind: Service
+metadata:
+  name: nginx-blue-svc
+spec:
+  ports:
+  - port: 80
+    protocol: TCP
+    targetPort: 80
+  selector:
+    app: nginx-blue
+  type: ClusterIP
+---
+
+apiVersion: networking.k8s.io/v1beta1
+kind: Ingress
+metadata:
+  name: nginx-blue-ing
+  annotations:
+    kubernetes.io/ingress.class: blue
+    nginx.ingress.kubernetes.io/ingress.class: blue
+    nginx.ingress.kubernetes.io/use-regex: "true"
+    nginx.ingress.kubernetes.io/rewrite-target: /$1
+    nginx.ingress.kubernetes.io/ssl-redirect: "false"
+spec:
+  rules:
+  - http:
+       paths:
+       - backend:
+           serviceName: nginx-blue-svc
+           servicePort: 80
+         path: /(/|$)(.*)
+       - backend:
+           serviceName: nginx-blue-svc
+           servicePort: 80
+         path: /nginx(/|$)(.*)
 EOF
 fi
 
@@ -45,6 +116,7 @@ if [ $GREEN = "true" ]; then
 	# Use Helm to deploy an NGINX ingress controller
 	helm install ingress-green ingress-nginx/ingress-nginx -f - \
 	    --namespace nginx \
+            --set controller.ingressClass=green \
 	    --set controller.replicaCount=2 \
 	    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
 	    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
@@ -55,6 +127,76 @@ controller:
     annotations:
       service.beta.kubernetes.io/azure-load-balancer-internal: "true"
       service.beta.kubernetes.io/azure-load-balancer-internal-subnet: "clusteringressservices"
+EOF
+
+	cat <<EOF | kubectl apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-green-dep
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: nginx-green
+  template:
+    metadata:
+      labels:
+        app: nginx-green
+    spec:
+      nodeSelector:
+        nodepoolcolor: green
+        nodepoolmode: user
+      containers:
+      - image: nginxdemos/hello
+        name: nginx-green
+        ports:
+        - containerPort: 80
+        resources:
+          requests:
+            memory: "64Mi"
+            cpu: "250m"
+          limits:
+            memory: "128Mi"
+            cpu: "350m"
+---
+
+apiVersion: v1
+kind: Service
+metadata:
+  name: nginx-green-svc
+spec:
+  ports:
+  - port: 80
+    protocol: TCP
+    targetPort: 80
+  selector:
+    app: nginx-green
+  type: ClusterIP
+---
+
+apiVersion: networking.k8s.io/v1beta1
+kind: Ingress
+metadata:
+  name: nginx-green-ing
+  annotations:
+    kubernetes.io/ingress.class: green
+    nginx.ingress.kubernetes.io/ingress.class: green
+    nginx.ingress.kubernetes.io/use-regex: "true"
+    nginx.ingress.kubernetes.io/rewrite-target: /$1
+    nginx.ingress.kubernetes.io/ssl-redirect: "false"
+spec:
+  rules:
+  - http:
+       paths:
+       - backend:
+           serviceName: nginx-green-svc
+           servicePort: 80
+         path: /(/|$)(.*)
+       - backend:
+           serviceName: nginx-green-svc
+           servicePort: 80
+         path: /nginx(/|$)(.*)
 EOF
 fi
 
